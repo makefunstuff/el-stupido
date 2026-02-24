2edff4505daaf2758ebb9d7070f106acf4ba0fb5
Iurii Plugatariov <plugatariov@gmail.com>
2026-02-24 11:09:00 +0200

    fix .gitignore: exclude all target/ dirs, remove committed build artifacts

--- compiler/primitives/target/debug/deps/primitives-9a3f5cc4533b5a5c.d
+++
@@ -1,8 +0,0 @@
-/home/jurip/Vibes/el-stupido/compiler/primitives/target/debug/deps/primitives-9a3f5cc4533b5a5c.d: src/lib.rs src/grug.rs src/html.rs src/http.rs
-
-/home/jurip/Vibes/el-stupido/compiler/primitives/target/debug/deps/libprimitives-9a3f5cc4533b5a5c.rmeta: src/lib.rs src/grug.rs src/html.rs src/http.rs
-
-src/lib.rs:
-src/grug.rs:
-src/html.rs:
-src/http.rs:
